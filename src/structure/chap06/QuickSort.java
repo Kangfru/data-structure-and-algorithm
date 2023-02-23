b0b39494b010e26508a21764b673dde07a7035d9
Kangfru <kangfru@gmail.com>
2023-02-23 20:45:44 +0900

    Java sort

---
+++ src/structure/chap06/QuickSort.java
@@ -0,0 +1,60 @@
+package structure.chap06;
+
+import java.util.Scanner;
+
+public class QuickSort {
+
+    static void swap(int[] a, int idx1, int idx2) {
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+    }
+
+    static void quickSort(int[] a, int left, int right) {
+        int pl = left; // 왼쪽 커서
+        int pr = right; // 우측 커서
+        int x = a[(pl + pr) / 2]; // 피벗
+
+        System.out.printf("a[%d] ~ a[%d] : {", left, right);
+        for (int i = left; i < right; i++) {
+            System.out.printf("%d , ", a[i]);
+        }
+        System.out.printf("%d}\n", a[right]);
+
+        do {
+            while (a[pl] < x) pl++;
+            while (a[pr] > x) pr--;
+            if (pl <= pr)
+                swap(a, pl++, pr--);
+        } while (pl <= pr);
+
+        if (left < pr) quickSort(a, left, pr);
+        if (pl < right) quickSort(a, pl, right);
+    }
+
+    static void quickSort(int[] a, int n) {
+        quickSort(a, 0, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("퀵 정렬");
+        System.out.print("요솟수 : ");
+        int nx = stdIn.nextInt();
+        int[] x = new int[nx];
+
+        for (int i = 0; i < nx; i++) {
+            System.out.print("x[" + i + "] : ");
+            x[i] = stdIn.nextInt();
+        }
+//        quickSort(x, 0, nx - 1);
+        quickSort(x, nx);
+
+        System.out.println("정렬결과");
+        for (int i = 0; i < nx; i++) {
+            System.out.println( x[i]);
+        }
+    }
+
+}
---
+++ src/structure/chap06/ShellSort.java
@@ -0,0 +1,39 @@
+package structure.chap06;
+
+import java.util.Scanner;
+
+public class ShellSort {
+
+    static void shellSort(int[] a, int n) {
+        for (int h = n / 2; h > 0; h /= 2) {
+            for (int i = h; i < n; i++) {
+                int j;
+                int tmp = a[i];
+                for (j = i - h; j >= 0 && a[j] > tmp; j -= h) {
+                    a[j + h] = a[j];
+                }
+                a[j + h] = tmp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("셸 정렬");
+        System.out.print("요솟수");
+        int nx = stdIn.nextInt();
+        int[] x = new int[nx];
+
+        for (int i = 0; i < nx; i++) {
+            System.out.print("x [" + i + "] : ");
+            x[i] = stdIn.nextInt();
+        }
+
+        shellSort(x, nx);
+        for (int i = 0; i < nx; i++) {
+            System.out.print("x [" + i + "] : " + x[i]);
+        }
+    }
+
+}

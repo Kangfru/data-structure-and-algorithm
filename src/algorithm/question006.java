2491a69d32c8ea9ff0492774d523dfac189b8ee5
Kangfru <kangfru@gmail.com>
2023-07-16 23:53:44 +0900

    2023-07-09 boj 1940, 2018

---
+++ src/algorithm/question006.java
@@ -0,0 +1,34 @@
+package algorithm;
+
+import java.util.Scanner;
+
+// 백준 2018
+public class question006 {
+
+    public void solution() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int startIndex = 1;
+        int endIndex = 1;
+        int sum = 1;
+        int count = 1;
+        while (endIndex != n) {
+            if (sum == n) {
+                count++;
+                endIndex++;
+                sum += endIndex;
+            } else if (sum > n) {
+                sum -= startIndex;
+                startIndex++;
+            } else {
+                endIndex++;
+                sum += endIndex;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        new question006().solution();
+    }
+}
---
+++ src/algorithm/question007.java
@@ -0,0 +1,43 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 백준 1940
+public class question007 {
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 재료의 개수
+        int target = Integer.parseInt(br.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        Arrays.sort(arr);
+        int startIndex = 0;
+        int endIndex = n - 1;
+        int count = 0;
+        while (startIndex < endIndex) {
+            int currentSum = arr[startIndex] + arr[endIndex];
+            if (currentSum == target) {
+                count++;
+                startIndex++;
+                endIndex--;
+            } else if (currentSum > target) {
+                endIndex--;
+            } else {
+                startIndex++;
+            }
+        }
+        System.out.println(count);
+    }
+    public static void main(String[] args) throws IOException {
+        new question007().solution();
+    }
+
+}

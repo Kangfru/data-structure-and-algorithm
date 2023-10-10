d291ae37a052145adbcaa1270a2ff1319a1f66dd
Kangfru <kangfru@gmail.com>
2023-10-10 09:50:38 +0900

    2023-10-09 leetcode practice

--- src/algorithm/leetcode/MinimumAbsoluteDifferenceInBst.java
+++ src/algorithm/leetcode/MinimumAbsoluteDifferenceInBst.java
@@ -1,4 +1,49 @@
 package algorithm.leetcode;
 
 public class MinimumAbsoluteDifferenceInBst {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    boolean init;
+    int min;
+    int prev;
+
+    public int getMinimumDifference(TreeNode root) {
+        init = false;
+        min = Integer.MAX_VALUE;
+        // Binary 이진트리를 inorder로 순회하면 오름차순이 됨.
+        inorder(root);
+        return min;
+    }
+
+    public void inorder(TreeNode node) {
+        if (node == null) return;
+        inorder(node.left);
+        // self
+        if (!init) {
+            init = true;
+        } else {
+            min = Math.min(min, node.val - prev);
+        }
+        prev = node.val;
+        inorder(node.right);
+    }
+
 }
